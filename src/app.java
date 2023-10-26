b0b24d90f5a779b02b5d6bdd130fc3a038e3d30d
Nikhil-Capillary <nikhil.sharma@capillarytech.com>
2023-10-26 14:47:50 +0530

    dev5

--- src/app.java
+++ src/app.java
@@ -1,6 +1,7 @@
 class App {
     static void stringEqualityCheckNoncompliant(String string1, String string2) {
     System.out.println("The strings are equal.");
+    System.out.println("The strings are equal.");
     }
 }
     public static void main(String[] args) {
