cc5f8981f6c8ff5ba50bbe50367aff4e206d5f46
Nikhil-Capillary <nikhil.sharma@capillarytech.com>
2023-10-26 13:48:12 +0530

    main

--- src/app.java
+++ src/app.java
@@ -1,8 +1,6 @@
 class App {
     static void stringEqualityCheckNoncompliant(String string1, String string2) {
-    // Noncompliant: the == operator doesn't compare the contents of the strings.
-    if(string1 == string2) {
-        System.out.println("The strings are equal.");
+    System.out.println("The strings are equal.");
     }
 }
     public static void main(String[] args) {
