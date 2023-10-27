3a09611a292fb16603315f74f62043939aa5632a
BONY MONDAL <bony.mondal@capillarytech.com>
2023-10-27 14:37:55 +0530

    test

--- src/test.java
+++ src/test.java
@@ -4,12 +4,14 @@ import java.sql.DriverManager;
 import java.util.Properties;
 class Test {
     public void createSqlConnectionNoncompliant() throws Exception {
-    // Noncompliant: password is hardcoded.
-    final Connection connection = DriverManager.getConnection("some url",
-            "username", "password");
-    connection.close();
-}
+        // Bad practice: Hardcoded database connection parameters.
+        final Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb",
+                "root", "insecure_password");
+        connection.close();
+    }
+
     public static void main(String[] args) {
+        // Bad practice: Empty main method, does nothing.
         return;
     }
 }
