7bd11a470dcf4fb23a586796d9fcdb9796a20a51
Nikhil-Capillary <nikhil.sharma@capillarytech.com>
2023-10-27 17:35:44 +0530

    change

--- src/dev6.java
+++ src/dev6.java
@@ -1,4 +1,10 @@
-private Double nullCheckPointerNoncompliant(@Nullable Double digit) {
+class Dev6{
+    private Double nullCheckPointerNoncompliant(@Nullable Double digit) {
     // Noncompliant: avoids null checks before dereferencing the pointer.
     return digit + 1.0;
+    }
+    public static void main(String[] args) {
+        System.out.println("The strings are equal.");
+    }
 }
+
